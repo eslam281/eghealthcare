a38308432ef816b008015107926274bdf8f2f2c6
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2026-02-08 21:24:06 +0200

    setup for the project add firebase

--- lib/core/bootstrap/app_bootstrap.dart
+++ lib/core/bootstrap/app_bootstrap.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../../firebase_options.dart';
 import '../../injection_container.dart';
 import '../configs/app_configs.dart';
 import '../services/cipher_key.dart';
 
 class AppBootstrap {
   static Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     const envString = String.fromEnvironment('ENV', defaultValue: 'dev');
     final env = getEnvironmentFromString(envString);
     AppConfig.initialize(env);
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../domain/repository/auth_reepository.dart';
+
 part 'auth_event.dart';
 part 'auth_state.dart';
-
+//
 // class AuthBloc extends Bloc<AuthEvent, AuthState> {
 //   final AuthRepository repo;
 //
