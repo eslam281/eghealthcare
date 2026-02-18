4e8ff6b2ef5daa178140342ef4a42b47bda32df2
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2026-02-18 21:32:52 +0200

    edit drawer

---
+++ lib/features/Appointments/domain/entities/appointment_entity.dart
@@ -0,0 +1,13 @@
+class AppointmentEntity {
+  final String doctorName;
+  final String type;
+  final String date;
+  final String time;
+
+  AppointmentEntity({
+    required this.doctorName,
+    required this.type,
+    required this.date,
+    required this.time,
+  });
+}
