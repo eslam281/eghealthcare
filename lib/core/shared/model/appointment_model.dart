a8aab9d3c39d7b084d6edc99f19fa14e7c10c9d2
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2026-04-19 17:53:46 +0200

    some edit in architecture and fix

---
+++ lib/core/shared/model/appointment_model.dart
@@ -0,0 +1,49 @@
+import 'package:eghealthcare/core/shared/model/doctor_model.dart';
+import 'package:eghealthcare/core/shared/model/patient_model.dart';
+
+class AppointmentModel {
+  late  int appointmentID;
+  late  DoctorModel doctor;
+  late PatientModel patient;
+  late String date;
+  late String time;
+  late String status;
+  late String createdAt;
+  late String updatedAt;
+
+  AppointmentModel(
+      {required this.appointmentID,
+        required this.doctor,
+        required this.patient,
+        required this.date,
+        required this.time,
+        required this.status,
+        required this.createdAt,
+        required this.updatedAt});
+
+  AppointmentModel.fromJson(Map<String, dynamic> json) {
+    appointmentID = json['appointmentID'];
+    doctor = DoctorModel.fromJson(json['doctor']);
+    patient = PatientModel.fromJson(json['patient']);
+    date = json['date'];
+    time = json['time'];
+    status = json['status'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['appointmentID'] = appointmentID;
+    data['doctor'] = doctor.toJson();
+    data['patient'] = patient.toJson();
+    data['date'] = date;
+    data['time'] = time;
+    data['status'] = status;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
+    return data;
+  }
+}
+
+
--- lib/features/Patient/Dashboard/data/models/patient_model.dart
+++ lib/features/Patient/Dashboard/data/models/patient_model.dart
@@ -1,77 +1,7 @@
+import '../../../../../core/shared/model/patient_model.dart';
 import '../../domain/entities/user_entity.dart';
 
-class PatientModel {
-  late String patientID;
-  late String name;
-  late int age;
-  late String gender;
-  late String phoneNumber;
-  late String email;
-  late String address;
-  String? avatar;
-  String? medicalHistory;
-  String? medicalSummary;
-  String? patientStats;
-  String? currentCondition;
-  String? medications;
-  String? createdAt;
-  String? updatedAt;
 
-  PatientModel(
-      {required this.patientID,
-        required this.name,
-        required this.age,
-        required this.gender,
-        required this.phoneNumber,
-        required this.email,
-        required this.address,
-        this.avatar,
-        this.medicalHistory,
-        this.medicalSummary,
-        this.patientStats,
-        this.currentCondition,
-        this.medications,
-        this.createdAt,
-        this.updatedAt});
-
-  PatientModel.fromJson(Map<String, dynamic> json) {
-    patientID = json['patientID'];
-    name = json['name'];
-    age = json['age'];
-    gender = json['gender'];
-    phoneNumber = json['phoneNumber'];
-    avatar = json['avatar'];
-    email = json['email'];
-    address = json['address'];
-    medicalHistory = json['medicalHistory'];
-    medicalSummary = json['medicalSummary'];
-    patientStats = json['patientStats'];
-    currentCondition = json['currentCondition'];
-    medications = json['medications'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['patientID'] = patientID;
-    data['name'] = name;
-    data['age'] = age;
-    data['gender'] = gender;
-    data['phoneNumber'] = phoneNumber;
-    data['email'] = email;
-    data['avatar'] = avatar;
-    data['address'] = address;
-    data['medicalHistory'] = medicalHistory;
-    data['medicalSummary'] = medicalSummary;
-    data['patientStats'] = patientStats;
-    data['currentCondition'] = currentCondition;
-    data['medications'] = medications;
-    data['createdAt'] = createdAt;
-    data['updatedAt'] = updatedAt;
-    return data;
-  }
-}
 extension PatientModelX on PatientModel{
   UserEntity toPatientEntity(){
     return UserEntity(
